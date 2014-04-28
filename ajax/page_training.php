02c5fe9ce42e642402034fddd3dcce134b6e51e5
Vykthur <chuvidi2003@gmail.com>
2014-04-28 19:09:29 +0800

    Improved Training Page

---
+++ ajax/page_training.php
@@ -0,0 +1,96 @@
+<?php require_once('../Connections/conncardsorter.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+mysql_select_db($database_conncardsorter, $conncardsorter);
+$query_rsconstructs = "SELECT * FROM constructs";
+$rsconstructs = mysql_query($query_rsconstructs, $conncardsorter) or die(mysql_error());
+$row_rsconstructs = mysql_fetch_assoc($rsconstructs);
+$totalRows_rsconstructs = mysql_num_rows($rsconstructs);
+?>
+<div class="row">
+	<div id="breadcrumb" class="col-md-12">
+		<ol class="breadcrumb">
+			<li><a href="index.html">Dashboard</a></li>
+			<li><a href="#">Pages</a></li>
+			<li><a href="#">Training Exercise </a></li>
+		</ol>
+	</div>
+</div>
+
+<div class="row">
+					<div class="col-xs-12">
+					  <div class="box">
+					    <div class="box-content">
+						  <p class="page-header"><strong>Progress </strong> <code> Card 1 of 5 </code></p>
+								 
+					    </div>
+					  </div>
+					</div>
+</div>
+<div class="row">
+  <div class="col-xs-12 ">
+	  <div class="box box-pricing">
+			<div class="box-header">
+				<div class="box-name">
+					<span>8GBPerfomance</span>
+				</div>
+				<div class="no-move"></div>
+			</div>
+			<div class="box-content no-padding">
+				<div class="row-fluid centered">
+					<div class="col-sm-12">8GB RAM</div>
+					<div class="col-sm-12">8 vCPU</div>
+					<div class="col-sm-12">80GB SSD</div>
+					<div class="col-sm-12">800Mb/s network bandwith</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="col-xs-12 bg-default darkborder">
+					<div class="col-sm-12">
+						
+						<?php do { ?>
+					    <div class="checkbox col-sm-3">
+						    <label>
+						      <input type="checkbox" class="form-control" data-toggle="tooltip"  title="Tooltip for name"> <?php echo $row_rsconstructs['title']; ?>
+						      <i class="fa fa-square-o"></i>
+						      </label>
+					      </div>
+						  <?php } while ($row_rsconstructs = mysql_fetch_assoc($rsconstructs)); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+mysql_free_result($rsconstructs);
+?>
