147223f6dd458ae25de5f70eb022a8fcaa6b7553
Victor D <chuvidi2003@gmail.com>
2015-11-04 12:53:58 +0800

    Implement JSON data parsing

--- savedata.php
+++ savedata.php
@@ -1,2 +1,24 @@
 <?php include("restrict.php"); ?>
-<?php echo "data saved!" ?>
+<?php 
+
+//echo "data saved!" ;
+if(isset($_POST['responsedata'])) {
+    $json = json_decode($_POST['responsedata'], true);
+   // print_r($json) ;
+   foreach($json as $rowid => $content) {
+    //in here you have the key in $keyName and the value in $value
+     // echo $rowid ; //= $value;
+	 foreach ($content as $value){
+	 	echo $rowid . " - " . $value["id"].":".$value["state"] . " |  " ;
+	 }
+	 echo "\n" ;
+   }	
+ } else {
+    echo "Noooooooob";
+ }
+ 
+function ifRecordExists(){
+	
+}
+
+ ?>
